a75e730fde158096c6420d86e02b450f56628a05
Andrei Rybak <rybak.a.v@gmail.com>
2020-04-22 21:33:21 +0200

    AudioPlayer: stop leaking resources
    Class Clip is AutoCloseable, however, we can't use try-with-resources
    here, because we actually want to hear the sounds---closing the Clip
    stops playback.  So collect all the Clips we are attempting to play into
    a list and close them after a short delay.

--- src/main/java/dev/andrybak/resoday/AudioPlayer.java
+++ src/main/java/dev/andrybak/resoday/AudioPlayer.java
@@ -5,28 +5,53 @@ import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.swing.Timer;
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.time.Duration;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 class AudioPlayer implements YearHistoryListener {
 	private static final String POSITIVE_AUDIO = "positive.wav";
 	private static final String NEGATIVE_AUDIO = "negative.wav";
+	/**
+	 * Assume that all audio clips are shorter than 10 seconds.
+	 */
+	private static final Duration AUDIO_TIMEOUT = Duration.ofSeconds(10);
 
-	private static void playSound(String resourceName) {
+	private final Timer closeTimer;
+	private List<Clip> toClose = new ArrayList<>();
+
+	AudioPlayer() {
+		closeTimer = new Timer((int)AUDIO_TIMEOUT.toMillis(), ignored -> {
+			for (Clip c : toClose)
+				c.close(); // avoid leaking OS audio resources
+			toClose.clear();
+		});
+		closeTimer.setRepeats(false);
+	}
+
+	private void playSound(String resourceName) {
 		System.out.println("Playing sound: " + resourceName);
+		Clip c = null;
 		try (
 			InputStream resource = Main.class.getResourceAsStream(resourceName);
 			BufferedInputStream buffered = new BufferedInputStream(resource);
 			AudioInputStream a = AudioSystem.getAudioInputStream(buffered);
 		) {
-			Clip c = AudioSystem.getClip(null);
+			c = AudioSystem.getClip(null);
 			c.open(a);
 			c.start();
 		} catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
 			System.err.println("Could not play sound.");
 			e.printStackTrace();
+		} finally {
+			if (c != null)
+				toClose.add(c);
+			closeTimer.start();
 		}
 	}
 
