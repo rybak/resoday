76d5aac237c83644ec659ee9ad4fc8c8f4a8e91d
Andrei Rybak <rybak.a.v@gmail.com>
2021-04-30 21:15:41 +0200

    AudioPlayer: use LineListener to close stopped Clip
    Replace hacky solution with Timer with the proper way to release
    resources associated with the Clip.

--- src/main/java/dev/andrybak/resoday/AudioPlayer.java
+++ src/main/java/dev/andrybak/resoday/AudioPlayer.java
@@ -3,58 +3,51 @@ package dev.andrybak.resoday;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineEvent;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
-import javax.swing.Timer;
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.time.Duration;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Objects;
 
 class AudioPlayer implements YearHistoryListener {
 	private static final String POSITIVE_AUDIO = "positive.wav";
 	private static final String NEGATIVE_AUDIO = "negative.wav";
 	private static final int MAX_SOUNDS = 10;
-	/**
-	 * Assume that all audio clips are shorter than 10 seconds.
-	 */
-	private static final Duration AUDIO_TIMEOUT = Duration.ofSeconds(10);
-
-	private final Timer closeTimer;
-	private final List<Clip> toClose = new ArrayList<>();
-
-	AudioPlayer() {
-		closeTimer = new Timer(Math.toIntExact(AUDIO_TIMEOUT.toMillis()), ignored -> {
-			for (Clip c : toClose)
-				c.close(); // avoid leaking OS audio resources
-			toClose.clear();
-		});
-		closeTimer.setRepeats(false);
-	}
+	private int currentlyPlaying = 0;
 
 	private void playSound(String resourceName) {
-		if (toClose.size() >= MAX_SOUNDS)
+		if (currentlyPlaying >= MAX_SOUNDS)
 			return;
+		currentlyPlaying++;
 		System.out.println("Playing sound: " + resourceName);
 		Clip c = null;
 		try (
 			InputStream resource = Resoday.class.getResourceAsStream(resourceName);
-			BufferedInputStream buffered = new BufferedInputStream(resource);
+			BufferedInputStream buffered = new BufferedInputStream(Objects.requireNonNull(resource,
+				() -> "Could not find resource '" + resourceName + "'"
+			));
 			AudioInputStream a = AudioSystem.getAudioInputStream(buffered)
 		) {
-			c = AudioSystem.getClip(null);
+			c = (Clip)AudioSystem.getLine(new DataLine.Info(Clip.class, a.getFormat()));
 			c.open(a);
+			final Clip finalClip = c;
+			c.addLineListener(lineEvent -> {
+				if (lineEvent.getType() == LineEvent.Type.STOP) {
+					finalClip.close();
+					currentlyPlaying--;
+				}
+			});
 			c.start();
 		} catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+			if (c != null)
+				c.close(); // it's autocloseable, but we want sound to _play_ for its duration before closing
 			System.err.println("Could not play sound.");
 			e.printStackTrace();
-		} finally {
-			if (c != null)
-				toClose.add(c);
-			closeTimer.start();
+			currentlyPlaying--;
 		}
 	}
 
