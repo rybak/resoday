7cf62f58de6c471fee9a5aaa4ebb481be5fda01e
Andrei Rybak <rybak.a.v@gmail.com>
2021-11-29 22:04:58 +0100

    AboutDialog: extract URL handling into new class "Hyperlinks"
    Extract the code from class AboutDialog into new utility class
    "Hyperlinks".  At first this was supposed to be for using it in a new
    class LicenseDialog.  But then I decided to put the license information
    into the AboutDialog as well.  But having it separate is still nice.

---
+++ src/main/java/dev/andrybak/resoday/gui/Hyperlinks.java
@@ -0,0 +1,98 @@
+package dev.andrybak.resoday.gui;
+
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.JTextPane;
+import javax.swing.event.HyperlinkEvent;
+import java.awt.BorderLayout;
+import java.awt.Cursor;
+import java.awt.Desktop;
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
+/**
+ * Utility methods for handling hyperlinks in {@link JComponent}s.
+ */
+public final class Hyperlinks {
+	private static final String XDG_OPEN_EXECUTABLE = "xdg-open";
+
+	private Hyperlinks() {
+		throw new UnsupportedOperationException();
+	}
+
+	public static void setUpHyperlinkListener(JTextPane textPane) {
+		textPane.addHyperlinkListener(hyperlinkEvent -> {
+			HyperlinkEvent.EventType eventType = hyperlinkEvent.getEventType();
+			if (HyperlinkEvent.EventType.ENTERED.equals(eventType)) {
+				textPane.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+			} else if (HyperlinkEvent.EventType.EXITED.equals(eventType)) {
+				textPane.setCursor(Cursor.getDefaultCursor());
+			} else if (HyperlinkEvent.EventType.ACTIVATED.equals(eventType)) {
+				URL url = hyperlinkEvent.getURL();
+				showUrl(url, () -> showUrlFallback(textPane, url));
+			}
+		});
+	}
+
+	private static void showUrl(URL url, Runnable fallback) {
+		if (url == null) {
+			return;
+		}
+		if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
+			try {
+				Desktop.getDesktop().browse(url.toURI());
+			} catch (IOException | URISyntaxException e) {
+				throw new IllegalStateException("Bad URLs in about text", e);
+			} catch (UnsupportedOperationException e) {
+				fallback.run();
+			}
+		} else {
+			String pathEnvVar;
+			try {
+				pathEnvVar = System.getenv("PATH");
+			} catch (SecurityException ignored) {
+				fallback.run();
+				return;
+			}
+			if (pathEnvVar == null) {
+				pathEnvVar = "";
+			}
+			// https://stackoverflow.com/a/23539220/1083697
+			boolean existsInPath = Stream.of(pathEnvVar.split(Pattern.quote(File.pathSeparator)))
+				.map(Paths::get)
+				.anyMatch(path -> Files.isExecutable(path.resolve(XDG_OPEN_EXECUTABLE)));
+			if (existsInPath) {
+				try {
+					Runtime.getRuntime().exec(XDG_OPEN_EXECUTABLE + " " + url);
+				} catch (IOException ignored) {
+					fallback.run();
+				}
+			} else {
+				fallback.run();
+			}
+		}
+	}
+
+	private static void showUrlFallback(JComponent parent, URL url) {
+		System.out.println("Fallback for " + url);
+		JPanel content = new JPanel(new BorderLayout());
+		{
+			content.add(new JLabel("Could not open URL:"), BorderLayout.NORTH);
+		}
+		{
+			JTextField urlField = new JTextField(url.toString());
+			urlField.setText(url.toString());
+			content.add(urlField, BorderLayout.CENTER);
+		}
+		JOptionPane.showMessageDialog(parent, content);
+	}
+}
--- src/main/java/dev/andrybak/resoday/gui/help/AboutDialog.java
+++ src/main/java/dev/andrybak/resoday/gui/help/AboutDialog.java
@@ -2,22 +2,16 @@ package dev.andrybak.resoday.gui.help;
 
 import dev.andrybak.resoday.StringConstants;
 import dev.andrybak.resoday.gui.Dialogs;
+import dev.andrybak.resoday.gui.Hyperlinks;
 
 import javax.swing.Box;
 import javax.swing.BoxLayout;
-import javax.swing.JComponent;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
 import javax.swing.JTextPane;
 import javax.swing.WindowConstants;
-import javax.swing.event.HyperlinkEvent;
-import java.awt.BorderLayout;
-import java.awt.Cursor;
-import java.awt.Desktop;
 import java.awt.Dialog;
 import java.awt.Dimension;
 import java.awt.Font;
@@ -25,17 +19,9 @@ import java.awt.Toolkit;
 import java.awt.Window;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.io.File;
 import java.io.IOException;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.regex.Pattern;
-import java.util.stream.Stream;
 
 public final class AboutDialog {
-	private static final String XDG_OPEN_EXECUTABLE = "xdg-open";
 	private static final String ABOUT_HTML_RESOURCE_FILENAME = "about.html";
 
 	private static JDialog create(Window parent) {
@@ -64,7 +50,7 @@ public final class AboutDialog {
 			e.printStackTrace();
 		}
 		textPane.setEditable(false);
-		setUpHyperlinkListener(textPane);
+		Hyperlinks.setUpHyperlinkListener(textPane);
 		textPane.setBackground(label.getBackground());
 		content.add(textPane);
 		content.add(Box.createVerticalStrut(10));
@@ -84,73 +70,6 @@ public final class AboutDialog {
 	public static void show(Window parent) {
 		JDialog d = create(parent);
 		d.setVisible(true);
-	}
-
-	private static void setUpHyperlinkListener(JTextPane textPane) {
-		textPane.addHyperlinkListener(hyperlinkEvent -> {
-			HyperlinkEvent.EventType eventType = hyperlinkEvent.getEventType();
-			if (HyperlinkEvent.EventType.ENTERED.equals(eventType)) {
-				textPane.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-			} else if (HyperlinkEvent.EventType.EXITED.equals(eventType)) {
-				textPane.setCursor(Cursor.getDefaultCursor());
-			} else if (HyperlinkEvent.EventType.ACTIVATED.equals(eventType)) {
-				URL url = hyperlinkEvent.getURL();
-				showUrl(url, () -> showUrlFallback(textPane, url));
-			}
-		});
-	}
-
-	private static void showUrl(URL url, Runnable fallback) {
-		if (url == null) {
-			return;
-		}
-		if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
-			try {
-				Desktop.getDesktop().browse(url.toURI());
-			} catch (IOException | URISyntaxException e) {
-				throw new IllegalStateException("Bad URLs in about text", e);
-			} catch (UnsupportedOperationException e) {
-				fallback.run();
-			}
-		} else {
-			String pathEnvVar;
-			try {
-				pathEnvVar = System.getenv("PATH");
-			} catch (SecurityException ignored) {
-				fallback.run();
-				return;
-			}
-			if (pathEnvVar == null) {
-				pathEnvVar = "";
-			}
-			// https://stackoverflow.com/a/23539220/1083697
-			boolean existsInPath = Stream.of(pathEnvVar.split(Pattern.quote(File.pathSeparator)))
-				.map(Paths::get)
-				.anyMatch(path -> Files.isExecutable(path.resolve(XDG_OPEN_EXECUTABLE)));
-			if (existsInPath) {
-				try {
-					Runtime.getRuntime().exec(XDG_OPEN_EXECUTABLE + " " + url);
-				} catch (IOException ignored) {
-					fallback.run();
-				}
-			} else {
-				fallback.run();
-			}
-		}
-	}
-
-	private static void showUrlFallback(JComponent parent, URL url) {
-		System.out.println("Fallback for " + url);
-		JPanel content = new JPanel(new BorderLayout());
-		{
-			content.add(new JLabel("Could not open URL:"), BorderLayout.NORTH);
-		}
-		{
-			JTextField urlField = new JTextField(url.toString());
-			urlField.setText(url.toString());
-			content.add(urlField, BorderLayout.CENTER);
-		}
-		JOptionPane.showMessageDialog(parent, content);
 	}
 
 	/**
