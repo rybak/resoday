ca49d11ca0846a5469c6bf84879e42b73172d7a5
Andrei Rybak <rybak.a.v@gmail.com>
2022-03-05 19:24:23 +0100

    Version: extract constant DIRTY_DEVELOPMENT_BUILD
    Extract constant String DIRTY_DEVELOPMENT_BUILD in class Version to be
    reused in the following commits.

--- src/main/java/dev/andrybak/resoday/gui/help/Version.java
+++ src/main/java/dev/andrybak/resoday/gui/help/Version.java
@@ -1,11 +1,12 @@
 package dev.andrybak.resoday.gui.help;
 
 public class Version {
+	private static final String DIRTY_DEVELOPMENT_BUILD = "dirty development build";
 
 	public static String extractVersion() {
 		String versionFromPackage = versionFromPackage();
 		if (versionFromPackage == null) {
-			return "dirty development build";
+			return DIRTY_DEVELOPMENT_BUILD;
 		}
 		return versionFromPackage;
 	}
