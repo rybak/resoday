ab01ac9e1eaf09cc00625c96b3a0b9acf6017dfd
Andrei Rybak <rybak.a.v@gmail.com>
2022-04-14 09:40:20 +0200

    SortOrderTest: use new collections static APIs

--- src/test/java/dev/andrybak/resoday/SortOrderTest.java
+++ src/test/java/dev/andrybak/resoday/SortOrderTest.java
@@ -2,81 +2,83 @@ package dev.andrybak.resoday;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
 
+import static java.util.stream.Collectors.toSet;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SortOrderTest {
 	@Test
 	void testThatWholeOrderWorks() {
-		Map<String, String> m = new HashMap<>();
-		m.put("third", "1234567890");
-		m.put("second", "Foobar");
-		m.put("first", "Hello");
-		SortOrder sortOrder = new SortOrder(Arrays.asList(
+		Map<String, String> m = Map.of(
+			"third", "1234567890",
+			"second", "Foobar",
+			"first", "Hello"
+		);
+		SortOrder sortOrder = new SortOrder(List.of(
 			"first",
 			"second",
 			"third"
 		));
-		List<String> expected = Arrays.asList("Hello", "Foobar", "1234567890");
-		List<String> actual = sortOrder.order(m).collect(Collectors.toList());
+		List<String> expected = List.of("Hello", "Foobar", "1234567890");
+		List<String> actual = sortOrder.order(m).toList();
 		assertEquals(expected, actual);
 	}
 
 	@Test
 	void testThatOrderWithMissingElementsWorks() {
-		Map<String, String> m = new HashMap<>();
-		m.put("fourth", "The Fourth String");
-		m.put("third", "1234567890");
-		m.put("second", "Foobar");
-		m.put("first", "Hello");
-		SortOrder sortOrder = new SortOrder(Arrays.asList(
+		Map<String, String> m = Map.of(
+			"fourth", "The Fourth String",
+			"third", "1234567890",
+			"second", "Foobar",
+			"first", "Hello"
+		);
+		SortOrder sortOrder = new SortOrder(List.of(
 			"first",
 			"second"
 		));
-		List<String> expectedHalf = Arrays.asList("Hello", "Foobar");
-		List<String> actual = sortOrder.order(m).collect(Collectors.toList());
+		List<String> expectedHalf = List.of("Hello", "Foobar");
+		List<String> actual = sortOrder.order(m).toList();
 		assertEquals(expectedHalf, actual.subList(0, 2));
 		assertEquals(new HashSet<>(m.values()), new HashSet<>(actual), "The rest of values should also be in actual");
 	}
 
 	@Test
 	void testThatOrderWithExtraElementsWorks() {
-		Map<String, String> m = new HashMap<>();
-		m.put("third", "1234567890");
-		m.put("second", "Foobar");
-		m.put("first", "Hello");
-		SortOrder sortOrder = new SortOrder(Arrays.asList(
+		Map<String, String> m = Map.of(
+			"third", "1234567890",
+			"second", "Foobar",
+			"first", "Hello"
+		);
+		SortOrder sortOrder = new SortOrder(List.of(
 			"first",
 			"second",
 			"third",
 			"ninth (does not exist)",
 			"tenth (does not exist)"
 		));
-		List<String> expected = Arrays.asList("Hello", "Foobar", "1234567890");
-		List<String> actual = sortOrder.order(m).collect(Collectors.toList());
+		List<String> expected = List.of("Hello", "Foobar", "1234567890");
+		List<String> actual = sortOrder.order(m).toList();
 		assertEquals(expected, actual);
 	}
 
 	@Test
 	void testThatOrderWithAllWrongElementsWorks() {
-		Map<String, String> m = new HashMap<>();
-		m.put("third", "1234567890");
-		m.put("second", "Foobar");
-		m.put("first", "Hello");
-		SortOrder sortOrder = new SortOrder(Arrays.asList(
+		Map<String, String> m = Map.of(
+			"third", "1234567890",
+			"second", "Foobar",
+			"first", "Hello"
+		);
+		SortOrder sortOrder = new SortOrder(List.of(
 			"eighth (does not exist)",
 			"ninth (does not exist)",
 			"tenth (does not exist)"
 		));
-		Set<String> expected = new HashSet<>(Arrays.asList("Hello", "Foobar", "1234567890"));
-		Set<String> actual = sortOrder.order(m).collect(Collectors.toSet());
+		Set<String> expected = Set.of("Hello", "Foobar", "1234567890");
+		Set<String> actual = sortOrder.order(m).collect(toSet());
 		assertEquals(expected, actual);
 	}
 }
