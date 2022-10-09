db997c7a386a83a61dbd4893bae451cc88edd026
Andrei Rybak <rybak.a.v@gmail.com>
2022-10-09 19:36:41 +0200

    Logo: extract class from MainGui

---
+++ src/main/java/dev/andrybak/resoday/gui/Logo.java
@@ -0,0 +1,15 @@
+package dev.andrybak.resoday.gui;
+
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.net.URL;
+import java.util.Objects;
+
+public class Logo {
+	private static final String APP_ICON_FILENAME = "resoday-icon.png";
+
+	public static Image getResodayImage() {
+		URL resodayIconUrl = Objects.requireNonNull(MainGui.class.getResource(APP_ICON_FILENAME));
+		return Toolkit.getDefaultToolkit().getImage(resodayIconUrl);
+	}
+}
