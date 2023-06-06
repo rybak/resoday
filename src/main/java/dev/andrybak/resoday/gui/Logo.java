d5f342f15453482ab792b30cf3102d62ded6d4e1
Andrei Rybak <rybak.a.v@gmail.com>
2023-06-06 11:39:01 +0200

    [WIP] windows blurry icons fix
    Windows seems like 32 pixel wide images for JFrame better.
    Need to test this on macOS too.
    
    It seems that AboutDialog doesn't like BaseMultiResolutionImage

--- src/main/java/dev/andrybak/resoday/gui/Logo.java
+++ src/main/java/dev/andrybak/resoday/gui/Logo.java
@@ -2,18 +2,23 @@ package dev.andrybak.resoday.gui;
 
 import java.awt.Image;
 import java.awt.Toolkit;
+import java.awt.image.BaseMultiResolutionImage;
 import java.net.URL;
 import java.util.Objects;
 
 public class Logo {
-	private static final String APP_ICON_FILENAME = "resoday-icon.png";
+	private static final String APP_ICON_64_FILENAME = "resoday-icon-64px.png";
+	private static final String APP_ICON_32_FILENAME = "resoday-icon-32px.png";
 
 	private Logo() {
 		throw new UnsupportedOperationException();
 	}
 
 	public static Image getResodayImage() {
-		URL resodayIconUrl = Objects.requireNonNull(MainGui.class.getResource(APP_ICON_FILENAME));
-		return Toolkit.getDefaultToolkit().getImage(resodayIconUrl);
+		URL resodayIcon64Url = Objects.requireNonNull(MainGui.class.getResource(APP_ICON_64_FILENAME));
+		URL resodayIcon32Url = Objects.requireNonNull(MainGui.class.getResource(APP_ICON_32_FILENAME));
+		var icon64 = Toolkit.getDefaultToolkit().getImage(resodayIcon64Url);
+		var icon32 = Toolkit.getDefaultToolkit().getImage(resodayIcon32Url);
+		return new BaseMultiResolutionImage(icon32, icon64);
 	}
 }
