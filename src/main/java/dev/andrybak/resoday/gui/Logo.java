f79f84772ae673d70e35b44f46a76b0590b4357c
Andrei Rybak <rybak.a.v@gmail.com>
2023-07-09 01:17:50 +0200

    MainGui: fix blurry icon on Windows
    For some reason, the 64 by 64 pixels icon doesn't get downscaled
    correctly on Windows in the top left corner of the JFrame.  Use method
    setIconImages() that takes several `Image`s of different resolutions to
    address this issue.
    
    TODO: check if setIconImages() addresses the Windows blurriness issue.
    
    An alternative solution would be to set a BaseMultiResolutionImage [1]
    that consists of a 32x32 icon and a 64x64 icon as the icon for the
    JFrame to fix the icon scaling/rendering issue.  However, there is a JDK
    bug in Linux that breaks BaseMultiResolutionImage, see
    
        https://github.com/rybak/foobar/blob/master/baz/src/main/java/swing/MultiResolutionImageBug.java
    
    On Windows, on the other hand, BaseMultiResolutionImage is rendered
    differently to ToolkitImage.  Windows uses an icon of size 56x56 pixels.
    When BaseMultiResolutionImage is passed, an image is constructed by
    drawing my 64x64 variant as a 48x48 image into a BufferedImage of size
    56x56.  See method sun.awt.SunToolkit#getScaledIconImage.
    
    [1] BaseMultiResolutionImage is from Java 9, while setIconImages() is
        from Java 6.

--- src/main/java/dev/andrybak/resoday/gui/Logo.java
+++ src/main/java/dev/andrybak/resoday/gui/Logo.java
@@ -2,18 +2,32 @@ package dev.andrybak.resoday.gui;
 
 import java.awt.Image;
 import java.awt.Toolkit;
+import java.awt.image.BaseMultiResolutionImage;
 import java.net.URL;
+import java.util.List;
 import java.util.Objects;
 
 public class Logo {
 	private static final String APP_ICON_64_FILENAME = "resoday-icon-64px.png";
+	private static final String APP_ICON_32_FILENAME = "resoday-icon-32px.png";
 
 	private Logo() {
 		throw new UnsupportedOperationException();
 	}
 
 	public static Image getFixedResolutionImage() {
-		URL resodayIconUrl = Objects.requireNonNull(MainGui.class.getResource(APP_ICON_64_FILENAME));
-		return Toolkit.getDefaultToolkit().getImage(resodayIconUrl);
+		return getImage(APP_ICON_64_FILENAME);
+	}
+
+	public static List<Image> getImages() {
+		return List.of(
+			getImage(APP_ICON_32_FILENAME),
+			getFixedResolutionImage()
+		);
+	}
+
+	private static Image getImage(String filename) {
+		URL url = Objects.requireNonNull(MainGui.class.getResource(filename));
+		return Toolkit.getDefaultToolkit().getImage(url);
 	}
 }
