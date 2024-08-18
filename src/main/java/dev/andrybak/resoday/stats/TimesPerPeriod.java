f8e98e1984eedc41ff905e32301e5f1e88c8999b
Andrei Rybak <rybak.a.v@gmail.com>
2024-08-18 23:36:27 +0200

    Stats: fix yet unused TimesPerPeriod

--- src/main/java/dev/andrybak/resoday/stats/TimesPerPeriod.java
+++ src/main/java/dev/andrybak/resoday/stats/TimesPerPeriod.java
@@ -1,6 +1,6 @@
 package dev.andrybak.resoday.stats;
 
-public record TimesPerPeriod(PeriodType periodType) implements Rate {
+public record TimesPerPeriod(long count, PeriodType periodType) implements Rate {
 	public TimesPerPeriod {
 		if (periodType == PeriodType.DAY) {
 			throw new IllegalArgumentException("TimesPerPeriod cannot take DAY as parameter in Resoday");
